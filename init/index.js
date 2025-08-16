2c02b5ac9ba1d427336dee7f6ddf12bb959e88c9
foxtrot222 <tirthkavathiya@gmail.com>
2025-08-16 22:06:31 +0530

    Add a feature to get mongodb link from .env

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const mongoUrl = "mongodb://127.0.0.1:27017/WanderStay";
+require('dotenv').config();
+const mongoUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
